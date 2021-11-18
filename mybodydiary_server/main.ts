86eb9e6b70924ff5a2e3d20dcf237caf60af0f5b
N22103011-JHR <n22103011-jhr@N22103011-JHR.local>
2021-11-18 10:24:34 +0900

    Add select user_info.

--- mybodydiary_server/main.ts
+++ mybodydiary_server/main.ts
@@ -1,14 +1,34 @@
-class Server {
-  private name: string;
+import * as dotenv from 'dotenv'
+import { Request, Response, NextFunction } from 'express'
+import express from 'express'
+import cors from 'cors'
+import { sequelize } from './models'
 
-  constructor(name: string) {
-    this.name = name;
-  }
- 
-  greet() {
-    console.log(`Hi! I am ${this.name}`);
-  }
-}
+dotenv.config()
 
-const server = new Server("hongten");
-server.greet();
+const PORT:number = parseInt(process.env.PORT as string, 10) || 3000
+const HOST:string = process.env.HOST || 'localhost'
+const app = express()
+
+app.use(cors())
+app.use(express.json());
+app.use((req:Request,res:Response,next:NextFunction) => {
+    console.log(`Request Occur! ${req.method}, ${req.url}`)
+    next()
+})
+
+const userRouter = require('./routes/userRouter')
+app.use('/users',userRouter)
+
+app.listen(PORT,HOST,async () => {
+  console.log(`Server Listening on ${HOST}:${PORT}`);
+
+  // //sequelize-db 연결 테스트
+   await sequelize.authenticate()
+   .then(async () => {
+       console.log("connection success");
+   })
+   .catch((e) => {
+       console.log('TT : ', e);
+   })
+})
